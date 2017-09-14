4f124b36124f2056af999d15c9e79617f0bb2446
defrox <defrostdj@hotmail.com>
2017-09-14 19:10:53 +0200

    Removed demo and updated version

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mvn clean package
-cd cas-overlay-server-demo
-docker build -t cas-overlay-server-demo .
-docker run --name server -p 127.0.0.1:8080:8080 -i -d cas-overlay-server-demo
-cd ../cas-overlay-management-demo/
-docker build -t cas-overlay-management-demo .
-docker run --name management -p 127.0.0.1:8081:8081 -i -d cas-overlay-management-demo
+cd cas-overlay-server
+docker build -t cas-overlay-server .
+docker run --name server -p 127.0.0.1:8080:8080 -i -d cas-overlay-server
+cd ../cas-overlay-management/
+docker build -t cas-overlay-management .
+docker run --name management -p 127.0.0.1:8081:8081 -i -d cas-overlay-management
